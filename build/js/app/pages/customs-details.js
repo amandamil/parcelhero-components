9a80595f08da3f7c39689aa790a6f372cf056bc2
dev8855 <anastasiiadmytrenko0@gmail.com>
2019-11-18 13:52:14 -0400

    Add UI Functions for Export Reason Select

--- build/js/app/pages/customs-details.js
+++ build/js/app/pages/customs-details.js
@@ -1,10 +1,10 @@
 function changeQuantityCount(){
-  let quantityCountBlock = document.getElementsByClassName('quantity-input-block');
+  const quantityCountBlock = document.getElementsByClassName('quantity-input-block');
 
   for(let block of quantityCountBlock){
-      let minusBtn = block.querySelector('.quantity-minus');
-      let plusBtn = block.querySelector('.quantity-plus');
-      let quantityInput = block.querySelector('.quantity-count');
+      const minusBtn = block.querySelector('.quantity-minus');
+      const plusBtn = block.querySelector('.quantity-plus');
+      const quantityInput = block.querySelector('.quantity-count');
 
       if (minusBtn.querySelector('.disabled') && parseInt(quantityInput.value) > 0) {
           minusBtn.querySelector('.fa-circle').classList.remove('disabled');
@@ -27,8 +27,66 @@ function changeQuantityCount(){
               minusBtn.querySelector('.fa-circle').classList.remove('disabled');
           }
       }
-
   }
 }
 
 changeQuantityCount();
+
+function changeExportReason() {
+  const exportReasonSelect = document.getElementById('export-reason');
+
+  const saleOptionText = document.getElementById('sale-option-text');
+  const saleOptionEditLink = document.getElementById('sale-option-edit');
+  const saleOptionEditor = document.getElementById('sale-option-editor');
+
+  saleOptionEditLink.onclick = function () {
+    saleOptionEditor.style.display = "block";
+    saleOptionText.style.display = "none";
+    saleOptionEditLink.style.display = "none";
+    saleOptionEditor.focus();
+  }
+
+  saleOptionEditor.onblur = function () {
+    saleOptionEditor.style.display = "none";
+    saleOptionText.style.display = "block";
+    saleOptionEditLink.style.display = "block";
+
+    saleOptionText.innerHTML = saleOptionEditor.value;
+  }
+
+  exportReasonSelect.onchange = function () {
+    hideOptionElements('sale-option');
+    hideOptionElements('personal-gift-option');
+    hideOptionElements('other-reason');
+
+    switch(exportReasonSelect.value) {
+      case 'sale':
+        showOptionElements('sale-option');
+        break;
+      case 'personal-gift':
+        showOptionElements('personal-gift-option');
+        break;
+      case 'other-reason':
+        break;
+      default:
+    }
+  }
+}
+
+function hideOptionElements(optionName) {
+  const optionElements = document.getElementsByClassName(optionName);
+
+  for(let element of optionElements) {
+    element.style.display = "none";
+  }
+}
+
+function showOptionElements(optionName) {
+  const optionElements = document.getElementsByClassName(optionName);
+
+  for(let element of optionElements) {
+    element.style.display = "block";
+  }
+}
+
+changeExportReason();
